45a7f776255e03c07ab4b244952900a76db593eb
Eli Goberdon <goberdon@gmail.com>
2018-11-02 19:38:32 -0400

    updated readme and added mode to introduce a near earth object

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # wadjet
+
 a python library for working with nasa open apis
+
+Currently wadjet only has one mode but I would love to add many.
+
+(Please send PRs with cool ones.)
+
+This mode finds the objects nearby in orbit, gets the first 12,
+selects one at random and introduces you to that space object.
+
+To get going 👇🏽
+
+`pip install -r requirements.txt`
+`python wadjet.py`
+
+example output:
+```
+There are 19668 total near earth objects at this time
+Lets check out 12 of them
+Digging further let's look @ a random one
+
+			~ 💫 Hello my real name is (1990 UN) ~
+
+			~ 🦄 But you can call me Zelaya San Alfonso de la Montaña 19 ~
+
+			~ 🐛 My number is 3092102 ~
+
+			~ 🐲 It has been said of me ~
+
+			~ ️🌚 nice, this near earth object is a superb friend  ~
+
+			~ ⛳ Check out some of my stats: ~
+
+			~ 📤 Max Diameter: 0.1185877909 kilometers ~
+
+			~ 📥 Min Diameter: 0.0530340723 kilometers ~
+
+```
+Runs on python 3.6
+Uses NASA's open API - NEO
--- wadjet.py
+++ wadjet.py
@@ -1,11 +1,121 @@
 import requests
 
+from faker import Faker
+from random import choice
 from credentials import API_KEY
 
-
-
 # hardcoding to near earth orbit stuff api for now
-HOUSTON = "https://api.nasa.gov/neo/rest/v1/neo/browse/"
+HOUSTON = "https://api.nasa.gov/neo/rest/v1/neo/browse"
 KEY = {'api_key': API_KEY}
-communique = requests.get(HOUSTON, data=KEY)
-import pdb; pdb.set_trace()
+NUM = 12
+
+
+def phone_home():
+
+	communique = requests.get(HOUSTON, params=KEY)
+	neo_data = communique.json()
+	return neo_data
+
+
+class NEO:
+	# instantiates the object of a near earth object
+	# an object object if you will heh
+
+	def __init__(self, neo_reference_id, designation, name, absolute_magnitude_h, estimated_diameter, is_potentially_hazardous_asteroid, is_sentry_object, **kwargs):
+
+		# pre loading stuff
+
+		self.system = System('metric')
+		self.fake = Faker('es_MX')
+
+		self.neo_id = neo_reference_id
+		self.designation = designation
+		self.name = name
+		self.better_name = self.get_better_name()
+		self.magnitude = absolute_magnitude_h
+		self.diameter = self.system.ingest(estimated_diameter)
+		self.sentry = is_sentry_object
+		self.nerd_stuff = kwargs
+
+		if is_potentially_hazardous_asteroid:
+			self.friend_or_foe = '☄️ uh oh, this space fella might be a foe '
+		else:
+			self.friend_or_foe = '🌚 nice, this near earth object is a superb friend '
+
+	def __repr__(self):
+
+		return self.better_name
+
+	def __str__(self):
+		key = 'kilometers' if self.system.metric else 'miles'
+
+		return f"""
+			~ 💫 Hello my real name is {self.name} ~\n
+			~ 🦄 But you can call me {self.better_name} ~ \n
+			~ 🐛 My number is {self.neo_id} ~ \n
+			~ 🐲 It has been said of me ~ \n
+			~ ️{self.friend_or_foe} ~ \n
+			~ ⛳ Check out some of my stats: ~ \n
+			~ 📤 Max Diameter: {self.diameter[key]['estimated_diameter_max']} {key} ~ \n
+			~ 📥 Min Diameter: {self.diameter[key]['estimated_diameter_min']} {key} ~ \n
+		"""
+
+	def get_better_name(self):
+		first = self.fake.last_name()
+		second = self.fake.city()
+		return f'{first} {second} {len(second) - len(first)}'
+
+
+class System:
+	# represents a measurement system
+	# Metric or British
+
+	def __init__(self, name):
+		metric = name.lower() == 'metric'
+		if not metric:
+			british = name.lower() in ['british', 'imperial']
+			if not british:
+				print(f"{name} system not found, using metric")
+				metric = True
+
+		self.metric = metric
+
+		if self.metric:
+			self.valid = ['kilometers', 'meters']
+		else:
+			self.valid = ['miles', 'feet']
+
+	def ingest(self, payload):
+		# takes a dict and returns in valid system
+
+		new_dict = {}
+
+		for k, v in payload.items():
+
+			if k in self.valid:
+
+				new_dict[k] = v
+
+		return new_dict
+
+
+def main():
+	neo_data = phone_home()
+	total_amount = neo_data['page']['total_elements']
+	print(f'There are {total_amount} total near earth objects at this time')
+	print(f'Lets check out {NUM} of them')
+	neos = []
+
+	counter = 0
+
+	for neo in neo_data['near_earth_objects']:
+		neos.append(NEO(**neo))
+		counter += 1
+		if counter > NUM:
+			break
+
+	print('Digging further let\'s look @ a random one')
+	print(choice(neos))
+
+
+main()
